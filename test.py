385bb9c583c2488d859a930668dfbcbe2bfe89c0
chathaha76 <83531336+chathaha76@users.noreply.github.com>
2021-05-02 18:40:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "AjbPHzj993dQM9Y0Rnv5QAvK6n4It2lI0cMLa4Bv"          # 본인 값으로 변경
-secret = "qTECHP2K3su9VKZl6cXqlbiTg09xSOQblphcyixx"          # 본인 값으로 변경
+access = ""          # 본인 값으로 변경
+secret = ""          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("DOGE"))     # KRW-DOGE 조회
